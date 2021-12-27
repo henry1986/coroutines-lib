63425a9b909f2080319ea2e85bcbb4210144086e
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2021-12-27 13:49:53 +0100

    ..

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 //        maven("https://artifactory.daiv.org/artifactory/gradle-dev-local")
     }
     dependencies {
-        classpath("org.daiv.dependency:DependencyHandling:0.0.155")
+        classpath("org.daiv.dependency:DependencyHandling:0.0.164")
     }
 }
 
