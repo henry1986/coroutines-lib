003da15714aebe9f593aacb74d0a5f38adf4cf18
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2023-03-11 17:01:34 +0100

    added TimeActor

--- src/commonTest/kotlin/org/daiv/coroutines/DataActorTest.kt
+++ src/commonTest/kotlin/org/daiv/coroutines/DataActorTest.kt
@@ -1,16 +1,105 @@
 package org.daiv.coroutines
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class DataActorTest {
-    data class StateX(val x:Int, val s:String)
+    data class StateX(val x: Int, val s: String)
+
     @Test
-    fun test() = runTest{
+    fun test() = runTest {
         val myDataActor = DataActor(StateX(9, "Hello"))
-        myDataActor.change { copy(5) }.join()
+        myDataActor.change { copy(x = 5) }.join()
         assertEquals(myDataActor.getData(), StateX(5, "Hello"))
         myDataActor.change { copy(s = "World") }.join()
-        assertEquals( StateX(5, "World"), myDataActor.getData())
+        assertEquals(StateX(5, "World"), myDataActor.getData())
+    }
+
+    @Test
+    fun testDelay() = runTest {
+        val timeGetter = object : TimeGetter {
+            override suspend fun currentTime(): Long {
+                return 500L
+            }
+        }
+        assertEquals(20L, timeGetter.getDelayTime(520L, "testTimeTriggerable"))
+        assertEquals(null, timeGetter.getDelayTime(499L, "testTimeTriggerable"))
+    }
+
+    data class StateTime(val _nextTime: Long? = null, val t: Int = 0) :
+        TimeTriggerable<StateTime> {
+        override fun getNextTime(): Long? {
+            return _nextTime
+        }
+
+        override fun runTimeEvent(time: Long): StateTime {
+            return copy(t = t + 1, _nextTime = null)
+        }
+
+        override fun isToCall(): Boolean {
+            return _nextTime != null
+        }
+    }
+
+    private fun check1(timeHandler: TimeHandler<StateTime>, t: StateTime) {
+        assertTrue(timeHandler.hasJob())
+        assertEquals(StateTime(520, 0), t)
+    }
+
+
+    @Test
+    fun testTimeActor() = runTest {
+        val suspendChannel = Channel<Any>()
+        val timeGetter = object : TimeGetter {
+            override suspend fun currentTime(): Long {
+                return 500L
+            }
+        }
+        val timeHandler: TimeHandler<StateTime> = TimeHandler(timeGetter) { suspendChannel.receive() }
+        val d = DataActor(StateTime(), timeHandler)
+        val c = Channel<StateTime>()
+        assertFalse(timeHandler.hasJob())
+        d.suspendChange { copy(_nextTime = 520L) }
+        d.suspendOnData {
+            c.send(this@suspendOnData)
+        }
+        check1(timeHandler, c.receive())
+        suspendChannel.send(Any())
+        delay(10)
+        d.suspendOnData {
+            c.send(this)
+        }
+        assertFalse(timeHandler.hasJob())
+        assertEquals(StateTime(null, 1), c.receive())
+    }
+
+    @Test
+    fun testTimeAbort() = runTest {
+        val suspendChannel = Channel<Any>()
+        val timeGetter = object : TimeGetter {
+            override suspend fun currentTime(): Long {
+                return 500L
+            }
+        }
+        val timeHandler: TimeHandler<StateTime> = TimeHandler(timeGetter) { suspendChannel.receive() }
+        val d = DataActor(StateTime(), timeHandler)
+        val c = Channel<StateTime>()
+        assertFalse(timeHandler.hasJob())
+        d.suspendChange { copy(_nextTime = 520L) }
+        d.suspendOnData {
+            c.send(this@suspendOnData)
+        }
+        check1(timeHandler, c.receive())
+        // abort counter
+        d.suspendChange { copy(_nextTime = null) }
+        d.suspendOnData {
+            c.send(this)
+        }
+        assertFalse(timeHandler.hasJob())
+        assertEquals(StateTime(null, 0), c.receive())
     }
 }
